1a86d9908bcaab8acb7d583e86e7e151b9091fdd
Dominic Aglialoro <domiaglialoro@gmail.com>
2024-04-25 16:32:00 -0700

    Import LevelTitleMessage

---
+++ Loenn/entities/levelTitleMessage.lua
@@ -0,0 +1,12 @@
+return {
+	name = "heavenRush/levelTitleMessage",
+	placements = {
+		name = "default",
+		data = {
+			subtitle = "",
+			title = ""
+		}
+	},
+	texture = "@Internal@/core_message",
+	depth = -1000000
+}
